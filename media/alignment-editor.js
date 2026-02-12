623abe9e17c16eb70db7157ce642e0451dddd339
Ming <oncwnuAUeYAFR4UGlq8BEJq8Jy-k@git.weixin.qq.com>
2026-02-12 21:30:08 +0800

    style: remove modified indicator to avoid obstructing text in editor

--- media/alignment-editor.js
+++ media/alignment-editor.js
@@ -265,15 +265,6 @@
             <textarea id="contentInput" 
               class="content-body-pro-input ${isModified ? 'border-modified' : ''}" 
               placeholder="Enter script content...">${seg.adjustedContent || seg.content}</textarea>
-            ${isModified ? `
-              <div class="modified-indicator" title="Text has been modified">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                </svg>
-                <span>Modified</span>
-              </div>
-            ` : ''}
           </div>
         </div>
       `;
